d6eb265aeb0cf2b1fb0226bd8c4ed656540e3abb
Ntoso Marcus <116955340+ntosomarcus@users.noreply.github.com>
2026-03-04 07:43:49 +0000

    added a line to update the dictionary key-name with the name of user

---
+++ test.py
@@ -0,0 +1,7 @@
+account_details[name] = {"name":  
+                                {"transaction": [] }
+                                }
+
+account_details[name] = "marcus"
+
+print(account_details)
